60b5a2dc7a5a9387783d0244869f37997884fbf2
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2020-01-13 11:03:13 +0000

    upd: voice animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", event => {
 
+    // Variables
     const container = document.querySelector('.container'),
         inputSearch = document.querySelector('.inputSearch'),
         input = document.querySelector('input'),
         voiceSearch = document.querySelector('.voiceSearch'),
         searchTitle = document.querySelector('.searchTitle');
 
-
+    // Events
     inputSearch.addEventListener('click', showInput);
     voiceSearch.addEventListener('click', hideInput);
 
@@ -14,8 +15,8 @@ window.addEventListener("load", event => {
     function showInput() {
         if (!container.classList.contains('showInput')) {
             container.classList.add('showInput');
+            voiceSearch.classList.remove('animeVoice');
             input.disabled = false;
-
 
             searchTitle.style.opacity = "0";
             searchTitle.style.transform = "translateX(-20px)";
@@ -39,13 +40,11 @@ window.addEventListener("load", event => {
                 searchTitle.style.opacity = "1";
                 searchTitle.style.transform = "translateX(0px)";
                 searchTitle.innerText = "Fale agora";
-            }, 400);
+            }, 500);
+
+            setTimeout(() => {
+                voiceSearch.classList.add('animeVoice');
+            }, 1200);
         }
-
-
-
     }
-
-
-
 });
